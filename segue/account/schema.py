da7bea0a6956dd46bfcf33ddfccd76124ee3731e
Ricardo Gerhardt <ric.gerhardt@gmail.com>
2016-01-12 09:15:57 -0200

    Updated account model fields

--- segue/account/schema.py
+++ segue/account/schema.py
@@ -9,19 +9,34 @@ signup = {
     "properties": {
         "email":        { "type": "string", "minLength": 5,  "maxLength": 80, "format": "email" },
         "name":         { "type": "string", "minLength": 5,  "maxLength": 80, "pattern": NAME_PATTERN},
+        "badge_name":         { "type": "string",  "maxLength": 80},
         "password":     { "type": "string", "minLength": 5,  "maxLength": 80 },
+        "password_confirm":     { "type": "string", "minLength": 5,  "maxLength": 80 },
         "disability":   { "type": "string", "enum": DISABILITY_TYPES },
         "disability_info": { "type": "string", "maxLenght": 200},
-        "cpf":          { "type": "string", "minLength": 11, "maxLength": 14, "pattern": CPF_PATTERN },
-        "document":     { "type": "string", "minLength": 5,  "maxLength": 14 },
-        "passport":     { "type": "string", "minLength": 5,  "maxLength": 15 },
-        "country":      { "type": "string", "minLength": 5,  "maxLength": 30 },
-        "city":         { "type": "string", "minLength": 3,  "maxLength": 30 },
+        "document":     { "type": "string", "minLength": 5,  "maxLength": 15 },
         "phone":        { "type": "string", "minLength": 5,  "maxLength": 30 },
         "organization": { "type": "string", "minLength": 3,  "maxLength": 80 },
         "resume":       { "type": "string", "minLength": 5,  "maxLength": 400 },
+        "occupation":      { "type": "string", "minLength": 1,  "maxLength": 20  },
+        "education":       { "type": "string", "minLength": 1,  "maxLength": 40  },
+        "sex":             { "type": "string", "minLength": 1,  "maxLength": 1  },
+        "born_date":       { "type": "string", "minLength": 10, "maxLenght": 10 },
+        "country":      { "type": "string", "minLength": 2,  "maxLength": 2 },
+        "city":         { "type": "string", "minLength": 3,  "maxLength": 30 },
+        "address_street":  { "type": "string", "minLength": 5,  "maxLength": 80  },
+        "address_number":  { "type": "string", "minLength": 1,  "maxLength": 20  },
+        "address_extra":   { "type": "string", "minLength": 0,  "maxLength": 40  },
+        "address_state":   { "type": "string", "minLength": 2,  "maxLength": 2  },
+        "address_neighborhood": { "type": "string", "minLength": 2,  "maxLength": 20  },
+        "address_zipcode": { "type": "string", "minLength": 2,  "maxLength": 10  },
+
     },
-    "required": ["email", "name", "password", "country", "city", "phone" ],
+    "required": [
+        "email", "name", "password", "disability", "document", "country", "city", "phone",
+        "address_street", "address_number", "address_state", "address_neighborhood",
+        "address_zipcode", "occupation", "education", "sex", "born_date"
+    ],
 }
 edit_account = signup.copy()
 edit_account['required'] = ["email", "name", "country", "city", "phone" ]
