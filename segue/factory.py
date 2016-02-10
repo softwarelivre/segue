89fb99b22c1b9afea17f251d7ef7c2bc9511d4d9
Software <inscricao@softwarelivre.org>
2016-02-09 23:12:26 -0200

    Added marshmallow

--- segue/factory.py
+++ segue/factory.py
@@ -1,8 +1,9 @@
 import jsonschema
 
 from segue.core import logger
+from segue.schema import BaseSchema
 
-from errors import SegueValidationError
+from errors import SegueValidationError, SchemaValidationError
 
 class Factory(object):
     @classmethod
@@ -11,13 +12,21 @@ class Factory(object):
 
     @classmethod
     def from_json(cls, data, schema):
-        data.pop('$type',None)
-        validator = jsonschema.Draft4Validator(schema, format_checker=jsonschema.FormatChecker())
-        errors = list(validator.iter_errors(data))
-        cleaned_data = cls.clean_for_insert(data)
-        if errors:
-            logger.error('validation error for %s.from_json: %s', cls, errors)
-            raise SegueValidationError(errors)
-        return cls.model(**cleaned_data)
+        data.pop('$type', None)
+        # TODO: REMOVE LATER
+        if isinstance(schema, BaseSchema):
+            parsed_data, errors = schema.load(data)
+            if errors:
+                raise SchemaValidationError(errors)
+            cleaned_data = cls.clean_for_insert(parsed_data)
+            return cls.model(**cleaned_data)
+        else:
+            validator = jsonschema.Draft4Validator(schema, format_checker=jsonschema.FormatChecker())
+            errors = list(validator.iter_errors(data))
+            cleaned_data = cls.clean_for_insert(data)
+            if errors:
+                logger.error('validation error for %s.from_json: %s', cls, errors)
+                raise SegueValidationError(errors)
+            return cls.model(**cleaned_data)
 
 
