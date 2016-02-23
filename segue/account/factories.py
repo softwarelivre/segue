50e0876255ea1f167e8c9b69344add978231c5bd
Software <inscricao@softwarelivre.org>
2016-02-22 22:12:12 -0300

    Improved account module

--- segue/account/factories.py
+++ segue/account/factories.py
@@ -8,20 +8,24 @@ class AccountFactory(Factory):
     model = Account
 
     UPDATE_WHITELIST = schema.signup["properties"].keys()
+    ITEMS_TO_REMOVE = ['type', 'password_confirm', 'email_confirm']
 
     @classmethod
     def clean_for_insert(cls, data):
-        data = { c:v for c,v in data.items() if c in AccountFactory.UPDATE_WHITELIST }
-        data['document'] = data.pop('cpf', None) or data.pop('passport', None) or data.pop('document', None)
-        return data;
+        for item in AccountFactory.ITEMS_TO_REMOVE:
+            if item in data:
+                data.pop(item)
+        data['document'] = data.pop('cpf', None) or data.pop('passport', None) or data.pop('document', None) or data.pop('cnpj', None)
+        return data
 
     @classmethod
     def clean_for_update(cls, data):
+        #TODO: FIX
         update_whitelist = AccountFactory.UPDATE_WHITELIST[:]
+        data['document'] = data.pop('cpf', None) or data.pop('passport', None) or data.pop('cnpj', None) or data.pop('document', None)
         update_whitelist.remove('email')
         data = { c:v for c,v in data.items() if c in update_whitelist }
-        data['document'] = data.pop('cpf', None) or data.pop('passport', None) or data.pop('document', None)
-        return data;
+        return data
 
 
 class ResetPasswordFactory(Factory):
