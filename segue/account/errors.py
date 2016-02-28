43f16da3122037ca8560678aa1e907921881a17d
Ricardo Gerhardt <ric.gerhardt@gmail.com>
2016-02-28 18:53:29 -0300

    Removed unused classes

--- segue/account/errors.py
+++ segue/account/errors.py
@@ -49,55 +49,6 @@ class EmailAlreadyInUse(SegueFieldError):
         self.value = email
 
 
-class InvalidDocumentNumber(SegueFieldError):
-
-    FIELD = 'document'
-    LABEL = 'invalid_format'
-    MESSAGE = 'invalid document number'
-
-    def __init__(self, document):
-        super(InvalidDocumentNumber, self).__init__()
-        self.value = document
-
-
-class InvalidDateFormat(SegueFieldError):
-
-    FIELD = 'born_date'
-    LABEL = 'invalid_format'
-    MESSAGE = 'invalid date format'
-
-    def __init__(self, born_date):
-        super(InvalidDateFormat, self).__init__()
-        self.value = born_date
-
-
-class PasswordMismatch(SegueFieldError):
-    FIELD = 'password_confirm'
-    LABEL = 'invalid_format'
-    MESSAGE = 'password mismatch'
-
-    def __init__(self, password):
-        super(PasswordMismatch, self).__init__()
-        self.value = password
-
-
-class InvalidAddress(SegueError):
-    code = 400
-
-    def to_json(self):
-        return {'message': u'Verifique os dados do endereço da conta'}
-
-
-class EmailMismatch(SegueFieldError):
-    FIELD = 'email_confirm'
-    LABEL = 'invalid_format'
-    MESSAGE = 'email mismatch'
-
-    def __init__(self, email):
-        super(EmailMismatch, self).__init__()
-        self.value = email
-
-
 class InvalidZipCodeNumber(FieldError):
     FIELD = 'address_zipcode'
     MESSAGE = _l('Invalid Zip Code number')
