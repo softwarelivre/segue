08bfc2838d03604bf8e2bdab9988d06e037d8911
Software <inscricao@softwarelivre.org>
2016-02-29 11:17:11 -0300

    Added a new buyer schema

--- segue/purchase/schema.py
+++ segue/purchase/schema.py
@@ -1,32 +1,82 @@
+import re
+from marshmallow import validates_schema, validates
+
+from errors import InvalidCNPJ, InvalidCPF, InvalidZipCodeNumber
+from segue.schema import BaseSchema, Field, Validator
+from segue.validation import CPFValidator, CNPJValidator, ZipCodeValidator
+
+
 BUYER_TYPES = ['person','company','government']
-CPF_CNPJ_PATTERN = r"(^\d{3}.?\d{3}.?\d{3}.?\d{2}$)|(^\d{2}.?\d{3}.?\d{3}.?\d{4}.?\d{2}$)"
-NAME_PATTERN = r"(.*)\s(.*)"
 
-#TODO: ADD AMOUNT FIELD FOR PRODUCTS WITHOUT PRICE
-buyer = {
-    "$schema": "http://json-schema.org/draft-04/schema#",
-    "type": "object",
-    "properties": {
-        "kind":            { "enum": BUYER_TYPES },
-        "name":            { "type": "string", "minLength": 5,  "maxLength": 100, "pattern": NAME_PATTERN },
-        "document":        { "type": "string", "minLength": 4, "maxLength": 20 }, #FIX
-        "contact":         { "type": "string", "minLength": 5,  "maxLength": 100 },
-        "address_street":  { "type": "string", "minLength": 5,  "maxLength": 80  },
-        "address_number":  { "type": "string", "minLength": 1,  "maxLength": 20  },
-        "address_extra":   { "type": "string", "maxLength": 40 },
-        "address_city":    { "type": "string", "minLength": 2,  "maxLength": 60  },
-        "address_country": { "type": "string", "minLength": 2,  "maxLength": 40  },
-        "address_state": { "type": "string", "minLength": 2,  "maxLength": 2  },
-        "address_neighborhood": { "type": "string", "minLength": 2,  "maxLength": 20  },
-        "address_zipcode": { "type": "string", "minLength": 2,  "maxLength": 10  },
-    },
-    "required": [
-        "kind", "name", "document", "contact", "address_street",
-        "address_number", "address_city", "address_country",
-        "address_neighborhood", "address_state", "address_zipcode"
-    ]
-}
+class BuyerSchema(BaseSchema):
 
+    kind = Field.str(
+        required=True,
+        validate=[Validator.one_of(choices=BUYER_TYPES)]
+    )
+    name = Field.str(
+        required=True,
+        validate=[Validator.length(min=5, max=80)]
+    )
+    contact = Field.str(
+        required=True,
+        validate=[Validator.length(min=5, max=30)]
+    )
+    cpf = Field.str()
+    cnpj = Field.str()
+    passport = Field.str()
+    document = Field.str(dump_only=True)
+
+    address_country = Field.str(
+        required=True,
+        validate=[Validator.length(min=3, max=20)]
+    )
+    address_city = Field.str(
+        required=True,
+        validate=[Validator.length(min=3, max=30)]
+    )
+    address_street = Field.str(
+        required=True,
+        validate=[Validator.length(min=5, max=80)]
+    )
+    address_number = Field.str(
+        required=True,
+        validate=[Validator.length(min=1, max=20)]
+    )
+    address_extra = Field.str(
+        validate=[Validator.length(max=40)]
+    )
+    address_state = Field.str(
+        required=True,
+        validate=[Validator.length(equal=2)]
+    )
+    address_neighborhood = Field.str(
+        required=True,
+        validate=[Validator.length(min=3, max=30)]
+    )
+    address_zipcode = Field.str(
+        required=True,
+        validate=[Validator.length(min=3, max=15)]
+    )
+
+    @validates('cpf')
+    def validate_cpf(self, data):
+        if not CPFValidator(data).is_valid():
+            raise InvalidCPF()
+
+    @validates('cnpj')
+    def validate_cnpj(self, data):
+        if not CNPJValidator(data).is_valid():
+            raise InvalidCNPJ()
+
+    @validates_schema()
+    def validate(self, data):
+        #TODO: IMPROVE
+        if re.match(r'br.*', data.get('country', ''), re.IGNORECASE):
+            if not ZipCodeValidator(data.get('address_zipcode', None)).is_valid():
+                raise InvalidZipCodeNumber()
+
+buyer = BuyerSchema()
 promocode = {
     "$schema": "http://json-schema.org/draft-04/schema#",
     "type": "object",
