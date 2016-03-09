0c45a76d7636cce0cf642f925b1417a316288ad4
Ricardo Gerhardt <ric.gerhardt@gmail.com>
2016-03-09 09:25:35 -0300

    Added more exceptions, implemented translation

--- segue/document/errors.py
+++ segue/document/errors.py
@@ -1,11 +1,25 @@
-from segue.errors import SegueError
+from segue.errors import SegueError, SegueGenericError
+from segue.babel import _l
 
 class DocumentNotFound(SegueError):
     code = 404
     def to_json(self):
-        return { 'message': 'could not find document {}'.format(self.args) }
+        return { 'message': _l('could not find document {}'.format(self.args)) }
+
+
+class InvalidDocument(SegueError):
+    code = 404
+    def to_json(self):
+        return {'message': _l('Invalid document format.')}
+
 
 class DocumentGenerationFailed(SegueError):
     code = 500
     def to_json(self):
-        return { 'message': 'document could no be generated' }
+        return {'message': _l('Document could not be generated.')}
+
+
+class DocumentSaveFailed(SegueError):
+    code = 500
+    def to_json(self):
+        return {'message': _l('Document could not be saved.')}
